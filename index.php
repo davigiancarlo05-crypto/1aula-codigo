b0b7a1877f7250c3321132544304779861348100
davigiancarlo05-crypto <davigiancarlo05@gmail.com>
2026-09-24 14:42:59 -0300

    g

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+$nome = "Davi";
+$idade = 17;
+$curso = "Desenvolvimento de Sistemas";
+
+echo "<h1>Sistema de Cadastro de Alunos</h1>";
+echo "Nome: " . $nome . "<br>";
+echo "Idade: " . $idade . "<br>";
+echo "Curso: " . $curso;
+
+?>
